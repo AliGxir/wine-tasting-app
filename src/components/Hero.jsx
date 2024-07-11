a5608130ca79f19dae3c7483d2545a4c8ceaa3b8
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-07-11 02:55:22 -0600

    remove unuse import in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
-
 const Hero = () => {
-
-    return (
-        <div className="hero-container">
-        <div className="hero-content">
-          <h1>Discover Your Perfect Wine</h1>
-          <h3>Track, Rate, and Take Notes of Your Wine Tasting Experiences</h3>
-          <div className="box">Mission: We aim to foster a community of wine lovers, helping users become informed and deepen their appreciation for the art of wine."</div>
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>Discover Your Perfect Wine</h1>
+        <h3>Track, Rate, and Take Notes of Your Wine Tasting Experiences</h3>
+        <div className="box">
+          Mission: We aim to foster a community of wine lovers, helping users
+          become informed and deepen their appreciation for the art of wine."
         </div>
-        <div className="hero-background"></div>
       </div>
-    )
-}
+      <div className="hero-background"></div>
+    </div>
+  );
+};
 
 export default Hero;
