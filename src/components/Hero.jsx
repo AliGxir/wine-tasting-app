16affdbb1f89be4dc89544577309e6ab23143589
AliGxir <147768928+AliGxir@users.noreply.github.com>
2024-07-11 03:32:37 -0600

    fix typo in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <h3>Track, Rate, and Take Notes of Your Wine Tasting Experiences</h3>
         <div className="box">
           Mission: We aim to foster a community of wine lovers, helping users
-          become informed and deepen their appreciation for the art of wine."
+          become informed and deepen their appreciation for the art of wine.
         </div>
       </div>
       <div className="hero-background"></div>
